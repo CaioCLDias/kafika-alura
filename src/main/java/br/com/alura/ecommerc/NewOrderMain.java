719335382d1ed9576c44196ba58c8c5a1da741b2
caio.dias <caio.dias@alaresinternet.com.br>
2024-01-24 15:59:38 -0300

    criando varios recptores

--- src/main/java/br/com/alura/ecommerc/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerc/NewOrderMain.java
@@ -1,26 +1,34 @@
 package br.com.alura.ecommerc;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
-        var value = "123,456,7899";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
-            if(ex != null) {
+        var key = UUID.randomUUID().toString();
+        var value = key + "123,456,7899";
+        var email = "Thank you for your order! We are processing your order";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
-            System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset "  + data.offset() + "/ timestamp " + data.timestamp());
-        }).get();
+            System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+        };
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
+
     }
 
     private static Properties properties() {
